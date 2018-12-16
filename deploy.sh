a209ef47377b3b020f98c1998532d487f4cf59d6
coneco <coneco@outlook.com>
2018-12-17 00:04:43 +0800

    add pip install at deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 ssh mxml@$1 'ps -ef | grep gunicorn | grep -v grep | cut -c 9-15 | xargs -r kill -9'
 ssh mxml@$1 'cd /home/mxml/mxml_server && git pull origin master'
+ssh mxml@$1 'cd /home/mxml/mxml_server/mxml && /home/mxml/anaconda3/bin/pip install -r requirements.txt'
 ssh mxml@$1 'cd /home/mxml/mxml_server/mxml && /home/mxml/anaconda3/bin/python manage.py collectstatic --noinput'
 ssh mxml@$1 'cd /home/mxml/mxml_server/mxml && /home/mxml/anaconda3/bin/python manage.py migrate --noinput'
 ssh mxml@$1 'cd /home/mxml/mxml_server/mxml && sh -c "nohup /home/mxml/anaconda3/bin/gunicorn mxml.wsgi > mxml.log 2>&1 &"'
