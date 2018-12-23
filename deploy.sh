7f2eb6c06ccfb4397744ca69b473ff5247573bfb
coneco <coneco@outlook.com>
2018-12-23 17:06:13 +0800

    add git clean in deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ssh mxml@$1 'ps -ef | grep gunicorn | grep -v grep | cut -c 9-15 | xargs -r kill -9'
 
 ssh mxml@$1 'cd /home/mxml/mxml_server && git pull origin master'
-ssh mxml@$1 'cd /home/mxml/mxml_server/mxml/mxml && rm -rf settings.py'
+ssh mxml@$1 'cd /home/mxml/mxml_server && git clean -f'
 scp ./mxml/mxml/settings_online.py mxml@$1:/home/mxml/mxml_server/mxml/mxml/settings.py
 
 ssh mxml@$1 'cd /home/mxml/mxml_server/mxml && /home/mxml/anaconda3/bin/pip install -r requirements.txt'
